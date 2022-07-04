a98647675938447fe06c29ab0b04ca471aef8930
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-07-04 19:03:02 -0300

    add method to update user points by login

--- src/DAO/ImplUsuarioDAO.java
+++ src/DAO/ImplUsuarioDAO.java
@@ -1,5 +1,9 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import Model.Usuario;
@@ -17,7 +21,18 @@ public class ImplUsuarioDAO implements UsuarioDAO {
 	@Override
 	public void adicionarPontos(String login, int pontos) {
 		
-
+		try (Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/usuarioDB", "root", "5432es")) {
+			String sql = "UPDATE usuario SET pontos = pontos + ? WHERE login = ?";
+			PreparedStatement stmt =  c.prepareStatement(sql);
+			
+			stmt.setInt(1, pontos);
+			stmt.setString(2, login);
+			
+			stmt.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
